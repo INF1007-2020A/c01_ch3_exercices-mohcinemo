1f9e545785cb58016e22b86c76900dd654aa8444
mohcinemo <71038285+mohcinemo@users.noreply.github.com>
2020-09-14 18:48:29 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -23,11 +23,11 @@ def to_degrees(angle_rads: float) -> tuple:
 
 
 def to_celsius(temperature: float) -> float:
-    return 1.8 * temperature +32
+    return ((temperature-32) * 1.8)
 
 
 def to_farenheit(temperature: float) -> float:
-    return (temperature-32) * 1.8
+    return (1.8 * temperature) +32
 
 
 def main() -> None:
