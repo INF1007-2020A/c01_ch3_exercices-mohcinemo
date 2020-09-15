315be36fca599ba05ce99489f8911ef83c266378
mohcinemo <mohcine.maher@polymtl.ca>
2020-09-15 20:00:15 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -27,7 +27,7 @@ def to_celsius(temperature: float) -> float:
 
 
 def to_farenheit(temperature: float) -> float:
-    return ((9* temperature) + 32)
+    return ((* temperature) + 32)
 
 
 def main() -> None:
